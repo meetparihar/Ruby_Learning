71b295cc32f7a7ad1ad447f75f69abc11f6041c9
meetparihar <meetpa9@gmail.com>
2022-04-01 17:14:39 +0530

    exception handling

--- day9/MapQuest/base.rb
+++ day9/MapQuest/base.rb
@@ -2,38 +2,69 @@ require_relative './APIs'
 require 'json'
 require 'faraday'
 require 'faraday/net_http'
+# require 'pry'
 module MapQuest
     class Base
         attr_reader :key        
         @@key = API_KEY
         def make_request(method, location, api)
-            @retries = 0
+            retries = 0
             begin                
                 params = {
                     'key' => @@key,
-                    'location' => location 
+                    # 'location' => location 
                 }
-                if method.downcase == "get"
-                    res = Faraday.get(api, params)    
+                # res = nil
+                if method.downcase == "get"                    
+                    res = Faraday.get(api, params)                    
+                    # puts res.status
                 elsif method.downcase == "post"
                     res = Faraday.post(api, params)
-                end                
-            rescue StandardError => e
-                if @retries < 3
-                    @retries += 1
-                    puts "retrying.."
-                    sleep(2) 
-                    retry
-                else
-                    raise e
                 end
+                
+                if res.status == 403
+                    raise " Wrong API, please check and try again "
+                end
+                puts res.status
+                ## Converting to hash because res.status gives 200 except 403
+                res_hash = JSON.parse(res.body)
+                if res_hash["info"]["statuscode"]/100 ==4
+                    raise "Wrong Location, Please check and try again"
+                end
+                if res_hash["info"]["statuscode"]/100 ==5
+                    raise "5"
+                end
+            rescue Exception => e
+                if(e.message=="5")
+                    if retries < 3
+                        retries += 1
+                        puts "retrying.."
+                        sleep(2) 
+                        retry
+                    else
+                        raise e
+                    end
+                end
+                puts e.message           
+                        
             end
+            return res
         end
     end
 end
 # res = MapQuest::Base.new.make_request("post","Denver,CO",FORWARD_API)
 # puts res.body
-
+# rescue Exception => e
+            #     puts e.message
+            #     # if retries < 3
+            #     #     retries += 1
+            #     #     puts "retrying.."
+            #     #     sleep(2) 
+            #     #     retry
+            #     # else
+            #     #     raise e
+            #     # end
+            # end
 # ## How to get status of res if error occured
 # retries = 0
 # begin
